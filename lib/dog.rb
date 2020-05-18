4be921cba55662075fae910477f2a04ce300aa22
Kevin Boston <kbos22@gmail.com>
2020-05-18 18:56:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,23 @@ class Dog
   
   attr_accessor
   
+  def initialize
+    @@all << self 
+    
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.print_all
+     @@all.each do |dog|
+       puts dog
+     end
+  end
+  
+  
+  
   
   
   
